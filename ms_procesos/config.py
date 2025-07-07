1242102d5e7c85e43773b8b357b4fdce6f48f060
leoKV <kevorvs@gmail.com>
2025-07-07 09:01:53 -0600

    UPDATE FIX

--- ms_procesos/config.py
+++ ms_procesos/config.py
@@ -1,14 +1,6 @@
 from pathlib import Path
 import environ
 
-# BASE_DIR = Path(__file__).resolve().parent.parent
-# env = environ.Env()
-# environ.Env.read_env(BASE_DIR / ".env", overwrite=True)
-
-# PATH_MAIN = env("PATH_MAIN", default="")
-# PATH_CREDENTIALS = env("PATH_CREDENTIALS", default="")
-# PATH_LOGS = env("PATH_LOGS", default="")
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 ENV_PATH = BASE_DIR / ".env"
 
